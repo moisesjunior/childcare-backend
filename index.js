c3cbaf1e7eb656bd2f653c7f3a72a7ac7d94e839
Moisés Junior <moises@jzci.com.br>
2020-05-31 11:03:27 -0300

    Arrumando config Heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const customExpress = require('./src/config/custom-express')
 const conexao = require('./src/config/connection')
 const Tabelas = require('./src/config/tabelas')
+require("dotenv").config()
 
 conexao.connect(erro => {
     if(erro){
