81e36ffc7dcb8f11a9c868cb159f67bb59f61654
Moisés Junior <nem.moises@hotmail.com>
2020-06-15 09:27:52 -0300

    Arrumando a conexão

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Única função do arquivo index.js é subir o servidor
 const customExpress = require('./src/config/custom-express')
 const conexao = require('./src/config/connection')
+const Tabelas = require('./src/config/tabelas')
 
 require("dotenv").config()
 
@@ -9,7 +10,7 @@ conexao.connect(erro => {
         console.log(erro)
     }
     const app = customExpress()
-    
+    Tabelas.init(conexao)
     let port = ""
     if(process.env.PORT){
         port = process.env.PORT
